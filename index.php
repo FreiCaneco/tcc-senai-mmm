0db36a092e3bbb24c0d8a2d19c37ed5bb5744823
MatheusCR <matheuscreinaldo07@gmail.com>
2025-09-16 11:39:56 -0300

    Base da navbar

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Organização Senai</title>
-    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <?php 
--- navbar.php
+++ navbar.php
@@ -1,14 +1,11 @@
 <!-- navbar.php -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg cor-principal">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Organização Senai</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <a class="navbar-brand cor-texto" href="index.php">Organização Senai</a>
+    <div id="navbarNav">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="index.php">Criar</a></li>
-        <li class="nav-item"><a class="nav-link" href="sobre.php">Pendências</a></li>
+        <li class="nav-item cor-texto"><a class="nav-link cor-texto" href="index.php">Criar</a></li>
+        <li class="nav-item cor-texto"><a class="nav-link cor-texto" href="sobre.php">Pendências</a></li>
       </ul>
     </div>
   </div>
