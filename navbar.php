6494a45536d928770df2d9af2078969453da04ef
MatheusCR <matheuscreinaldo07@gmail.com>
2025-09-16 10:50:19 -0300

    Commit inicial com navbar e ideia de estrutura para o banco de dados

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<!-- navbar.php -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Organização Senai</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="index.php">Criar</a></li>
+        <li class="nav-item"><a class="nav-link" href="sobre.php">Pendências</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
