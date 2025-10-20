10a9a5a8caf37ef0861a8b813b04fc868c1645aa
Matheus <mcreinaldo07@gmail.com>
2025-10-20 10:48:42 -0300

    fullcallendar no teste

--- view/teste.php
+++ view/teste.php
@@ -9,8 +9,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <!-- Fonte -->
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;600&display=swap" rel="stylesheet">
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.19/index.global.min.js'></script>
+    
 </head>
 <body>
     <?php include "../view/navbar.php" ?>
+
+    <div id='calendar'></div>
 </body>
+<script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
 </html>
